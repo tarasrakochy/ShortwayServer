b193a874d1f32ddbbdc8f13ab1c2fb54b12d7c89
tarkorti <tarkorti911@gmail.com>
2017-11-07 02:36:11 +0200

    change AppInit

---
+++ src/main/java/com/taras/shortway/server/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.taras.shortway.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.taras.shortway.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
