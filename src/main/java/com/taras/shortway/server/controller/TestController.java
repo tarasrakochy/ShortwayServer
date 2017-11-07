c730420cd599112bb8cfc5416b9b88ececbc3733
tarkorti <tarkorti911@gmail.com>
2017-11-07 21:49:40 +0200

    add functional to controller

--- src/main/java/com/taras/shortway/server/controller/TestController.java
+++ src/main/java/com/taras/shortway/server/controller/TestController.java
@@ -3,9 +3,11 @@ package com.taras.shortway.server.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/test")
+@ResponseBody
 public class TestController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
