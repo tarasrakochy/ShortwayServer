18363ff3c1720d2d9980a0b977098f67428ede6e
tarkorti <tarkorti911@gmail.com>
2017-11-13 15:58:50 +0200

    test object_mapper()

--- src/main/java/com/taras/shortway/server/controller/TestController.java
+++ src/main/java/com/taras/shortway/server/controller/TestController.java
@@ -1,17 +1,33 @@
 package com.taras.shortway.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.taras.shortway.server.entity.Auto;
+import com.taras.shortway.server.entity.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/test")
 @ResponseBody
 public class TestController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public String get() {
-        return "Hello, get";
+    public User get() {
+        Auto auto = new Auto();
+        auto.setId(1);
+        auto.setBrand("Mazda");
+        auto.setColor("Red");
+        auto.setCountry("Japan");
+        auto.setModel("RX-7");
+        auto.setNumber("333");
+        auto.setType("light");
+
+        User user = new User();
+        user.setAuto(auto);
+        user.setEmail("email");
+        user.setId(2);
+        user.setPhone("47-232");
+        return user;
     }
 }
